4fa40b8a081c6b4a44c361bbe50e7fd7a6ef91c2
Mateusz Piwek <Mateusz@teaFS.org>
2016-03-21 00:56:47 +0100

    Little fixes

--- face.js
+++ face.js
@@ -1,4 +1,5 @@
 
+
 function DigitalFace(min_id, sec_id) {
   var min_tag = document.getElementById(min_id);
   var sec_tag = document.getElementById(sec_id);
@@ -8,33 +9,30 @@ function DigitalFace(min_id, sec_id) {
     
     min_tag.innerHTML = '0' + time.getMin();
     sec_tag.innerHTML = '' + (((sec = time.getSec()) > 9) ? sec : '0' + sec);
-    
   };
 }
 
-function BarFace() {
+
+function AnalogBarFace() {
     var bar_id = document.getElementById('bar_indicator');
     var percentage = -1;
     
-     this.update = function (time) {
-         var new_percentage = Math.round(time.getAnalog() * 100);
+    this.update = function (time) {
+        var new_percentage = Math.round(time.getAnalog() * 100);
          
-         if(percentage != new_percentage) {
-             bar_id.style.width = (percentage = new_percentage) + '%';
-         }
-     }
+        if(percentage != new_percentage) {
+            bar_id.style.width = (percentage = new_percentage) + '%';
+        }
+    }
 }
 
-function PieFace() {
+function AnalogPieFace() {
     var pie_id = document.getElementById('pieSlice1');
     var deg;
-    
-   // console.log();
    
    this.update = function (time) {
     deg = 360 - (360 * time.getAnalog());
     //++deg;
     pie_id.style.transform = 'rotate(' + deg + 'deg)';
-
   };
 }
